850b80c147878ef9eae07fe4b706ed30f30de640
trigrou <trigrou@trigrou.com>
2020-12-26 23:47:13 +0100

    Adds clang-tidy to the CI

--- CMakeModules/format.cmake
+++ CMakeModules/format.cmake
@@ -31,3 +31,48 @@ if(CLANG_FORMAT)
 else()
     message(STATUS "clang-format not found disable target clang-format")
 endif()
+
+# Additional targets to perform clang-format/clang-tidy
+# Get all project files
+file(GLOB_RECURSE ALL_TIDY_FILES
+    src/*.cpp
+    )
+
+# Adding clang-tidy target if executable is found
+set(INCLUDE_DIRECTORIES_TIDY
+    -I/usr/local/include
+    -I/usr/include
+    -I${CMAKE_SOURCE_DIR}/deps/
+    )
+
+# Disables native clang tidy integration
+# even if user provides CMAKE_CXX_CLANG_TIDY
+set(CLANG_TIDY_NAME ${CMAKE_CXX_CLANG_TIDY})
+set(CMAKE_CXX_CLANG_TIDY "" CACHE STRING "" FORCE)
+
+find_program(CLANG_TIDY NAMES ${CLANG_TIDY_NAME} "clang-tidy-11" "clang-tidy")
+if(CLANG_TIDY)
+    add_custom_target(
+        clang-tidy
+        COMMAND ${CMAKE_SOURCE_DIR}/scripts/clang-tidy.sh
+        ${CLANG_TIDY}
+        8
+        ${ALL_TIDY_FILES}
+        )
+    message(STATUS "clang-tidy found: ${CLANG_TIDY}")
+else()
+    message(STATUS "clang-tidy not found disable target clang-tidy")
+endif()
+
+find_program(IWYU NAMES "iwyu_tool" "iwyu_tool.py")
+if(IWYU)
+    add_custom_target(
+        check-includes
+        COMMAND ${IWYU}
+        -p
+        .
+        )
+    message(STATUS "include-what-you-use found: ${IWYU} use: make check-includes")
+else()
+    message(STATUS "include-what-you-use not found, consider to install it to use check-includes\n apt-get -y install iwyu\n brew install iwyu")
+endif()
